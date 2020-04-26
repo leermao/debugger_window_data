d3305d282dd0b027e02f7639ec87fd3723a218b3
lipeng1 <447552708@qq.com>
2020-04-26 17:06:45 +0800

    注入css

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
 function injectScript(file, node) {
-  var th = document.getElementsByTagName(node)[0];
-  var s = document.createElement("script");
-  s.setAttribute("type", "text/javascript");
-  s.setAttribute("src", file);
-  th.appendChild(s);
+  var body = document.getElementsByTagName(node)[0];
+  var script = document.createElement("script");
+  script.setAttribute("type", "text/javascript");
+  script.setAttribute("src", file);
+  body.appendChild(script);
 }
 injectScript(chrome.extension.getURL("js/data.js"), "body");
+
+function injectStyle(url, node) {
+  const head = document.getElementsByTagName(node)[0];
+  const link = document.createElement("link", node);
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = url;
+  head.appendChild(link);
+}
+injectStyle(chrome.extension.getURL("./css/debugger.css"), "head");
