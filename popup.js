9894b2b2b6709d6fef6bd22ca4d4ba496fe8a266
liuzhaoxu <i-liuzhaoxu@360.cn>
2020-06-05 22:23:33 +0800

    合并到devtools工具

---
+++ popup.js
@@ -0,0 +1,8 @@
+// const bg = chrome.extension.getBackgroundPage();
+
+// const opts = document.getElementById('options').value;
+// alert(opts)
+// document.getElementById('addOptions').onClick = () => {
+//     alert(00)
+//     bg.setOptions(opts);
+// }
